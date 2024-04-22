7fc715316e8de9995da11886c731aaaccdf93dd8
Robson-oliveiraa <robsonpvh07@gmail.com>
2024-04-22 02:32:04 -0300

    Criação pag

---
+++ src/routes/mainwindows.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export default function MainWindows() {
+
+  return (
+    <>
+     <h1>Ola Mundo</h1>
+     <Link to={'homepage'}>
+      <Button variant='success'>Button Test</Button>
+     </Link>
+    </>
+  )
+}
