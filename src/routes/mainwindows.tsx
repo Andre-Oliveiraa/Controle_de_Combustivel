12dcac8120fb318426e23b2e39048bb5ae1747a3
WandellAZ <wandelldacostamota@gmail.com>
2024-04-22 00:45:54 -0400

    Commit

--- src/routes/mainwindows.tsx
+++ src/routes/mainwindows.tsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 export default function MainWindows() {
 
   return (
-    <>
+    <div>
      <h1>Ola Mundo</h1>
      <Link to={'homepage'}>
       <Button variant='success'>Button Test</Button>
      </Link>
-    </>
+    </div>
   )
 }
