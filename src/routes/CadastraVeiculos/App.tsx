04da45798a1d568906ae40038bdd0b72be70b30e
WandellAZ <wandelldacostamota@gmail.com>
2024-07-27 16:26:23 -0300

    Creator pag control

--- src/routes/CadastraVeiculos/App.tsx
+++ src/routes/CadastraVeiculos/App.tsx
@@ -1,12 +1,28 @@
 'use client'
 
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import { Car, Fuel as Fuelcon, CalendarDays, TicketSlash, ChevronLeft, Info, IterationCw } from 'lucide-react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HeaderIcon, HeaderRoot, HeaderTitle } from '../../components/header';
-import { ChevronLeft } from 'lucide-react'
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 
 export default function App() {
 
+  const [id, setId] = useState('');
+  const gerarId = () => {
+    const novoId = Math.floor(100000000 + Math.random() * 900000000);
+    setId(novoId.toString());
+  };
+  useEffect(() => {
+    gerarId();
+  }, []);
+
+  const fuel = ['Gasolina Comum', 'Gasolina Aditivada', 'Álcool - Etanol', 'Diesel']
+  
   return (
     <div className='h-screen bg-slate-100 font-outfit'>
       <HeaderRoot>
@@ -20,12 +36,114 @@ export default function App() {
         </Link>
       </div>
 
-      <div className='w-24 ml-4 '>
-        <Link to={'/SucessCadastro'} >
-          <button className='flex flex-row gap-2 p-1 text-red-500 border-red-300 border-2 w-24 items-center justify-center rounded-lg outline-none'>Button of Test </button>
-        </Link>
-      </div>
+      <div>
+        <form action="" className='grid grid-cols-2 grid-rows-2 gap-3 mx-[34rem]'>
+          <div>
+            <Form.Label className='font-bold'>Veiculos</Form.Label>
+            <InputGroup className="mb-5">
+              <InputGroup.Text><Car /></InputGroup.Text>
+              <FloatingLabel
+                label='Ex: Celta'
+              >
+                <Form.Control
+                  className='font-bold text-zinc-500 focus:text-zinc-500'
+                  type="text"
+                  placeholder='Celta'
+                  aria-describedby="Veiculo"
+                  required
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </div>
+          <div>
+            <Form.Label className='font-bold'>Ano</Form.Label>
+            <InputGroup className="mb-5">
+              <InputGroup.Text><CalendarDays /></InputGroup.Text>
+              <FloatingLabel
+                label='Ex: 21/22'
+              >
+                <Form.Control
+                  className='font-bold text-zinc-500 focus:text-zinc-500'
+                  type="text"
+                  placeholder='21/22'
+                  aria-describedby="Ano"
+                  required
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </div>
+          <div>
+            <Form.Label className='font-bold'>Placa</Form.Label>
+            <InputGroup className="mb-5">
+              <InputGroup.Text><TicketSlash /></InputGroup.Text>
+              <FloatingLabel
+                label='Ex: HTJ2346'
+              >
+                <Form.Control
+                  className='font-bold text-zinc-500 focus:text-zinc-500'
+                  type="text"
+                  placeholder='HTJ2346'
+                  aria-describedby="Placa"
+                  required
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </div>
+          <div>
+            <Form.Label className='font-bold'>Tipos de Combustível</Form.Label>
+            <InputGroup className="mb-5">
+              <InputGroup.Text><Fuelcon /></InputGroup.Text>
+              <FloatingLabel
+                label='Ex: Diesel'
+              >
+                <Form.Select
+                  aria-label="Default select example"
+                  className='font-bold text-zinc-500'
+                  defaultValue={''}
+                >
+                  <option selected className='font-bold text-zinc-500'>...</option>
+                  {fuel?.map(
+                    fuel => {
+                      return (
+                        <option value={fuel} className='font-bold text-zinc-500'>{fuel}</option>
+                      )
+                    }
+                  )}
+                </Form.Select>
+              </FloatingLabel>
+            </InputGroup>
+          </div>
+          <div>
+            <Form.Label className='font-bold'>ID</Form.Label>
+            <InputGroup className="mb-5 flex flex-row">
+              <InputGroup.Text><Info/></InputGroup.Text>
+              <FloatingLabel
+                label='Nº De Matricula'
+              >
+                <Form.Control
+                  className='font-bold text-zinc-500 focus:text-zinc-500'
+                  type="number"
+                  placeholder='Ex: 10L'
+                  aria-describedby="km final"
+                  disabled
+                  value={id}
+                />
+              </FloatingLabel>
+              <button onClick={gerarId} type={'button'}><IterationCw /></button>
+            </InputGroup>
+            <InputGroup className='flex flex-row items-center gap-2 justify-center'>
+              <InputGroup.Radio />
+              <Form.Label className='font-bold text-sm text-zinc-400'>Estou ciente da importância do n° de matricula</Form.Label>
+            </InputGroup>
+          </div>
 
+          <div className=' h-full ml-4 w-full flex flex-row justify-center items-center'>
+            <Link to={'/SucessCadastro'} >
+              <button type={'submit'} className='font-semibold text-zinc-200 rounded p-3 bg-indigo-400 border-none hover:bg-indigo-500 transition-all'>Cadastro de Veiculo </button>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- src/routes/ControleOfCombustivel/App.tsx
+++ src/routes/ControleOfCombustivel/App.tsx
@@ -2,273 +2,124 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HeaderIcon, HeaderRoot, HeaderTitle } from '../../components/header';
+import { ChevronLeft, BadgeCheck, Info, BadgeDollarSign } from 'lucide-react'
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import { Car, CircleFadingPlus, Trash2, Fuel as Fuelcon, HandCoins, Coins, Gauge, Construction, TicketSlash, ChevronLeft } from 'lucide-react'
-import { useState } from 'react';
-import { useForm } from 'react-hook-form'
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-
-interface PropsObjectInfo {
-  nameCar: string,
-  placaCar: string,
-  typeFuel: string
-  moneyLitro: string,
-  moneyFuel: string,
-  finaliKm: string,
-  initKm: string,
-}
 
 export default function App() {
-
-  const [infoAll, SetInfoAll] = useState<PropsObjectInfo>()
-  const [ValueFull, SetValueFull] = useState<any>()
-  const [ValueForPart, SetValueForPart] = useState<any>()
-  const [TotalValue, SetTotalValue] = useState<any>('0')
-  const [KmInit, SetKmInit] = useState<any>()
-  const [KmFinal, SetKmFinal] = useState<any>()
-  const [KmRodado, SetKmRodado] = useState<any>("0")
-
-  const { register, handleSubmit } = useForm()
-
-  async function handleReqiterSubmit(dataForm: any) {
-    SetInfoAll(dataForm)
-    console.log(infoAll)
-  }
-
-  const fuel = ['Gasolina Comum', 'Gasolina Aditivada', 'Álcool - Etanol', 'Diesel']
-
-  function TotalFull() {
-    var total = ValueFull / ValueForPart
-    SetTotalValue(total.toFixed(2))
-  }
-
-  function KmTotal() {
-    var total = KmFinal - KmInit
-    SetKmRodado(total.toFixed(2))
-  }
-
+  const [localPartida, setLocalPartida] = useState('');
+  const [localChegada, setLocalChegada] = useState('');
+  const [matricula, setMatricula] = useState('');
+  const [valorAbastecer, setValorAbastecer] = useState('');
+  const [id, setId] = useState('');
+  const [cadastroSucesso, setCadastroSucesso] = useState(false);
+  const [partida, setPartida] = useState('');
+  const [chegada, setChegada] = useState('');
+  const cidades = [
+    'São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Brasília',
+    'Curitiba',
+    'Porto Alegre',
+    'Recife',
+    'Salvador',
+    'Fortaleza',
+    'Goiânia',
+  ];
 
 
   return (
-    <div className='h-screen flex flex-col justify-between bg-slate-100 font-outfit'>
+    <div className='h-screen bg-slate-100 font-outfit'>
       <HeaderRoot>
         <HeaderIcon src='Logo.png' />
-        <HeaderTitle>Controle de Combustível</HeaderTitle>
+        <HeaderTitle>Controle de Combustivel</HeaderTitle>
       </HeaderRoot>
 
       <div className='w-24 ml-4 '>
         <Link to={'/'} >
-          <button className='flex flex-row gap-2 p-1 text-red-500 border-red-300 border-2 w-24 items-center justify-center rounded-lg outline-none'> <ChevronLeft /> <p className='outline-none'>Voltar</p></button>
+          <button className='flex flex-row gap-2 p-1 text-red-500 border-red-300 border-2 w-24 items-center justify-center rounded-lg'> <ChevronLeft/> <p className='hover:outline-none'>Voltar</p></button>
         </Link>
       </div>
 
-      {/* Home */}
-      <form onSubmit={handleSubmit(handleReqiterSubmit)} className='w-full items-center flex flex-col px-10'>
-        <div className='grid grid-cols-3 gap-3 w-full'>
-          <div >
-            <Form.Label className='font-bold'>Veiculos</Form.Label>
+      { cadastroSucesso ? (
+        <div className='font-semibold flex flex-row justify-center items-center gap-2 text-green-600 text-2xl'>
+          <BadgeCheck size={30}/>
+          <p>Cadastro Feito com Sucesso</p>
+        </div>
+      ) : (
+        <form className='grid grid-cols-1 gap-4 justify-center w-1/2 m-auto'>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <Form.Label>Local de Partida:</Form.Label>
+              <Form.Select value={partida} onChange={(e) => setPartida(e.target.value)}>
+                {cidades.map((cidade) => (
+                  <option key={cidade} value={cidade}>
+                    {cidade}
+                  </option>
+                ))}
+              </Form.Select>
+            </div>
+            <div>
+              <Form.Label>Local de Chegada:</Form.Label>
+              <Form.Select value={chegada} onChange={(e) => setChegada(e.target.value)}>
+                {cidades.map((cidade) => (
+                  <option key={cidade} value={cidade}>
+                    {cidade}
+                  </option>
+                ))}
+              </Form.Select>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+            <Form.Label className='font-bold'>Nº de Matricula de Carro</Form.Label>
             <InputGroup className="mb-5">
-              <InputGroup.Text><Car /></InputGroup.Text>
+              <InputGroup.Text><Info /></InputGroup.Text>
               <FloatingLabel
-                label='Ex: Celta'
+                label='174597841'
               >
                 <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
+                  className='font-bold text-zinc-500 focus:text-zinc-500 h-16'
                   type="text"
-                  placeholder='Celta'
-                  aria-describedby="km inicil"
+                  placeholder='174597841'
+                  aria-describedby="Nº de Matricula de Carro"
                   required
-                  {...register('nameCar')}
                 />
               </FloatingLabel>
             </InputGroup>
-          </div>
-          <div>
-            <Form.Label className='font-bold'>Placa</Form.Label>
+            </div>
+            <div>
+            <Form.Label className='font-bold'>Valor para Abastecer</Form.Label>
             <InputGroup className="mb-5">
-              <InputGroup.Text><TicketSlash /></InputGroup.Text>
+              <InputGroup.Text><BadgeDollarSign /></InputGroup.Text>
               <FloatingLabel
-                label='Ex: HTP6R54'
-              >
+                label='Ex: 100R$'
+                >  
                 <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500 uppercase'
-                  type="text"
-                  placeholder='JTR 1S89'
-                  required
-                  aria-describedby="km inicil"
-                  {...register('placaCar')}
-                />
-              </FloatingLabel>
-            </InputGroup>
-          </div>
-          <div>
-            <Form.Label className='font-bold'>Tipos de Combustível</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><Fuelcon /></InputGroup.Text>
-              <FloatingLabel
-                label='Ex: Diesel'
-              >
-                <Form.Select
-                  aria-label="Default select example"
-                  className='font-bold text-zinc-500'
-                  {...register('typeFuel')}
-                  defaultValue={''}
-                >
-                  <option selected className='font-bold text-zinc-500'>...</option>
-                  {fuel?.map(
-                    fuel => {
-                      return (
-                        <option value={fuel} className='font-bold text-zinc-500'>{fuel}</option>
-                      )
-                    }
-                  )}
-                </Form.Select>
-              </FloatingLabel>
-            </InputGroup>
-          </div>
-        </div>
-        <div className='grid grid-cols-3 gap-3 w-full'>
-          <div>
-            <Form.Label className='font-bold'>Valor Abastecido</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><HandCoins /></InputGroup.Text>
-              <FloatingLabel
-                label='Ex: R$ 150'
-              >
-                <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
-                  type="number"
-                  placeholder='Ex: R$100'
-                  aria-describedby="km inicil"
-                  required
-                  {...register('moneyFuel')}
                   min={0}
-                  onChange={e => SetValueFull(e.target.value)}
-                  pattern={"[0-9]+([,\.][0-9]+)?"}
-                  step={"any"}
-                />
-              </FloatingLabel>
-            </InputGroup>
-          </div>
-          <div>
-            <Form.Label className='font-bold'>Preço por Litro</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><Coins /></InputGroup.Text>
-              <FloatingLabel
-                label='Ex: R$5,4'
-              >
-                <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
+                  className='font-bold text-zinc-500 focus:text-zinc-500 h-16'
                   type="number"
-                  placeholder='Ex: R$5,9'
-                  aria-describedby="km final"
+                  placeholder='174597841'
+                  aria-describedby="Valor para Abastecer"
                   required
-                  {...register('moneyLitro')}
-                  min={0}
-                  onChange={e => SetValueForPart(e.target.value)}
-                  pattern={"[0-9]+([,\.][0-9]+)?"}
-                  step={"any"}
                 />
               </FloatingLabel>
             </InputGroup>
+            </div>
           </div>
-          <div>
-            <Form.Label className='font-bold'>Quantidade de Litro Abastecido</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><Gauge /></InputGroup.Text>
-              <FloatingLabel
-                label='Qnt. de Litro'
-              >
-                <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
-                  type="number"
-                  placeholder='Ex: 10L'
-                  aria-describedby="km final"
-                  disabled
-                  value={TotalValue}
-                />
-              </FloatingLabel>
-              <Button variant='success' onClick={TotalFull}>Calcular</Button>
-            </InputGroup>
-          </div>
-        </div>
-        <div className='grid grid-cols-3 gap-3 w-full'>
-          <div >
-            <Form.Label className='font-bold'>Km inicial</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><Car /></InputGroup.Text>
-              <FloatingLabel
-                label='Ex: 7km'
-              >
-                <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
-                  type="number"
-                  placeholder='Celta'
-                  aria-describedby="km inicil"
-                  required
-                  {...register('initKm')}
-                  min={0}
-
-                  onChange={(e) => SetKmInit(e.target.value)}
-                />
-              </FloatingLabel>
-            </InputGroup>
-          </div>
-          <div >
-            <Form.Label className='font-bold'>Km final</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><Car /></InputGroup.Text>
-              <FloatingLabel
-                label='Ex: 14km'
-              >
-                <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
-                  type="number"
-                  placeholder='Celta'
-                  aria-describedby="km inicil"
-                  required
-                  {...register('finaliKm')}
-                  min={0}
-
-                  onChange={e => SetKmFinal(e.target.value)}
-                />
-              </FloatingLabel>
-            </InputGroup>
-          </div>
-          <div>
-            <Form.Label className="font-bold">KM Rodado</Form.Label>
-            <InputGroup className="mb-5">
-              <InputGroup.Text><Construction /></InputGroup.Text>
-              <FloatingLabel
-                label='KM Rodado'
-              >
-                <Form.Control
-                  className='font-bold text-zinc-500 focus:text-zinc-500'
-                  type="number"
-                  aria-describedby="km final"
-                  disabled
-                  placeholder='0'
-                  value={KmRodado}
-                />
-              </FloatingLabel>
-              <Button variant='success' onClick={KmTotal}>Calcular</Button>
-            </InputGroup>
-          </div>
-        </div>
-        <div className='w-40 ml-auto'>
-          <Link to={'/SucessControl'}   >
-            <Button variant="success" className='mb-3 flex gap-2 items-center justify-center hover:text-green-200 w-full' type='submit'>
-              <CircleFadingPlus /><p>Cadastrar</p>
-            </Button>
+          <div className="flex justify-center">
+            <Link to={'/SucessControl'}>
+          <button type={'submit'} className='font-semibold text-zinc-200 rounded p-3 bg-indigo-400 border-none hover:bg-indigo-500 transition-all'>Cadastro de Veiculo </button>
           </Link>
-          <Button variant="danger" className='mb-3 flex gap-2 items-center justify-center hover:text-red-200 w-full' type='button'><Trash2 /><p>Limpar</p></Button>
-        </div>
-      </form>
+          </div>
+        </form>
+      )}
 
-
+      
     </div>
   )
 }
